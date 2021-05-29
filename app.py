01509f594b8fd46f417490ef49ef2bfa88e2111d
ShrutiSundar <68639875+ShrutiSundar@users.noreply.github.com>
2021-05-29 11:22:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 29 10:55:49 2021
+
+@author: shrut
+"""
+
+import numpy as np
+import pickle
+import joblib
+
+from flask import Flask, request, jsonify, render_template
+import base64
+import io
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+survmodel = pickle.load(open('survivemodel.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    gender = 0
+    if request.form["gender"] == 1:
+        gender = 1
+    SeniorCitizen = 0
+    if 'SeniorCitizen' in request.form:
+        SeniorCitizen = 1
+    Partner = 0
+    if 'Partner' in request.form:
+        Partner = 1
+    Dependents = 0
+    if 'Dependents' in request.form:
+        Dependents = 1
+    PaperlessBilling = 0
+    if 'PaperlessBilling' in request.form:
+        PaperlessBilling = 1
+
+    MonthlyCharges = int(request.form["MonthlyCharges"])
+    Tenure = int(request.form["Tenure"])
+    TotalCharges = MonthlyCharges*Tenure
+
+    PhoneService = 0
+    if 'PhoneService' in request.form:
+        PhoneService = 1
+
+    MultipleLines = 0
+    if 'MultipleLines' in request.form and PhoneService == 1:
+        MultipleLines = 1
+
+    InternetService_Fiberoptic = 0
+    InternetService_No = 0
+    if request.form["InternetService"] == 0:
+        InternetService_No = 1
+    elif request.form["InternetService"] == 2:
+        InternetService_Fiberoptic = 1
+
+    OnlineSecurity = 0
+    if 'OnlineSecurity' in request.form and InternetService_No == 0:
+        OnlineSecurity = 1
+
+    OnlineBackup = 0
+    if 'OnlineBackup' in request.form and InternetService_No == 0:
+        OnlineBackup = 1
+
+    DeviceProtection = 0
+    if 'DeviceProtection' in request.form and InternetService_No == 0:
+        DeviceProtection = 1
+
+    TechSupport = 0
+    if 'TechSupport' in request.form and InternetService_No == 0:
+        TechSupport = 1
+
+    StreamingTV = 0
+    if 'StreamingTV' in request.form and InternetService_No == 0:
+        StreamingTV = 1
+
+    StreamingMovies = 0
+    if 'StreamingMovies' in request.form and InternetService_No == 0:
+        StreamingMovies = 1
+
+    Contract_Oneyear = 0
+    Contract_Twoyear = 0
+    if request.form["Contract"] == 1:
+        Contract_Oneyear = 1
+    elif request.form["Contract"] == 2:
+        Contract_Twoyear = 1
+
+    PaymentMethod_CreditCard = 0
+    PaymentMethod_ElectronicCheck = 0
+    PaymentMethod_MailedCheck = 0
+    if request.form["PaymentMethod"] == 1:
+        PaymentMethod_CreditCard = 1
+    elif request.form["PaymentMethod"] == 2:
+        PaymentMethod_ElectronicCheck = 1
+    elif request.form["PaymentMethod"] == 3:
+        PaymentMethod_MailedCheck = 1
+
+    features = [gender, SeniorCitizen, Partner, Dependents, Tenure, PhoneService, MultipleLines, OnlineSecurity, OnlineBackup,
+       DeviceProtection, TechSupport, StreamingTV, StreamingMovies, PaperlessBilling, MonthlyCharges, TotalCharges,
+       InternetService_Fiberoptic, InternetService_No, Contract_Oneyear,Contract_Twoyear,
+       PaymentMethod_CreditCard, PaymentMethod_ElectronicCheck, PaymentMethod_MailedCheck]
+
+    columns = ['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure', 'PhoneService', 'MultipleLines', 'OnlineSecurity', 'OnlineBackup',
+       'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies','PaperlessBilling', 'MonthlyCharges', 'TotalCharges',
+       'InternetService_Fiber optic', 'InternetService_No', 'Contract_One year', 'Contract_Two year',
+       'PaymentMethod_Credit card (automatic)', 'PaymentMethod_Electronic check', 'PaymentMethod_Mailed check']
+
+    final_features = [np.array(features)]
+    prediction = model.predict_proba(final_features)
+
+    output = prediction[0,1]
+    
+    return render_template('index.html', prediction_text='Churn probability is {}'.format(round(output, 2)))
+
+if __name__ == "__main__":
+    app.run(debug=True)
